3e697c83718717968936d27e2e3dd9be0b08ad38
coding-to-music <connors.tom@gmail.com>
2022-04-07 05:22:24 +0000

    Heroku

--- server.js
+++ server.js
@@ -1,30 +1,35 @@
+// dotenv config
+require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
 const articleRouter = require("./routes/articles");
 const methodOverride = require("method-override");
 
 const app = express();
-const { PORT, mongoUri } = require("./config/config");
+// const { PORT, mongoUri } = require("./config/config");
 const path = require("path");
+const PORT = process.env.PORT || 3000;
+const mongoUri = process.env.MONGODB_URI;
 
 const article_controller = require("./controllers/article.controllers");
 
 mongoose.Promise = global.Promise;
 mongoose
-	.connect(mongoUri, {
-		useNewUrlParser: true,
-		useCreateIndex: true,
-		useUnifiedTopology: true,
-		useFindAndModify: false,
-	})
-	.then(
-		() => {
-			console.log("MongoDB database is connected");
-		},
-		(err) => {
-			console.log("Failed to connect to the database" + err);
-		}
-	);
+  .connect(mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then((x) => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo: ", err);
+  });
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
@@ -34,6 +39,10 @@ app.get("/", article_controller.article_list);
 app.use("/articles", articleRouter);
 app.use(express.static(path.join(__dirname, "public")));
 
+// app.listen(PORT, () => {
+//   console.log("Server is running on Port:", PORT);
+// });
+
 app.listen(PORT, () => {
-	console.log("Server is running on Port:", PORT);
+  console.log(`Server listening on port http://localhost:${PORT}`);
 });
